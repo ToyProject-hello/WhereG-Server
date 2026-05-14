d81d19ac1b099d9a5aad394a5f7d5884823b5c5c
minwoo-3 <s26002@gsm.hs.kr>
2026-05-14 12:17:46 +0900

    feat:

---
+++ src/main/java/org/example/whereg/WhereGApplication.java
@@ -0,0 +1,13 @@
+package org.example.whereg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereGApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/whereg/WhereGApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.whereg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereGApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
