bf796175e7742fdd348329c72f7f840d8ef0c435
master <master@MacBook-Pro.local>
2026-05-30 06:36:06 -0700

    fix: 잘못된 파일 경로 수정

--- WhereG/main/java/org/example/whereg/domain/auth/dto/request/SignInRequest.java
+++
@@ -1,6 +0,0 @@
-package org.example.whereg.domain.auth.dto.request;
-
-public record SignInRequest(
-        String password,
-        String email
-) {}
--- WhereG/main/java/org/example/whereg/domain/user/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package org.example.whereg.domain.user.enums;
-
-public enum Role {
-    ADMIN, USER
-}
