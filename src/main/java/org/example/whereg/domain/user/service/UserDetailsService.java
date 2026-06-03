4a62c938427f5f33e84e296862618440cea05aa2
SungWon Jung <s26059@gsm.hs.kr>
2026-06-03 23:27:34 +0900

    refactor: UserDetailsService 원복

--- src/main/java/org/example/whereg/domain/user/service/UserDetailsService.java
+++ src/main/java/org/example/whereg/domain/user/service/UserDetailsService.java
@@ -15,12 +15,7 @@ public class UserDetailsService implements org.springframework.security.core.use
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.getEmail())
-                .password(user.getPassword())
-                .roles(user.getRole().name())
-                .build();
     }
 }
